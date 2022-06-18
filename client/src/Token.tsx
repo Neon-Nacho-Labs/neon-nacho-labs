5ffdba175acc71d6671cfae6428edcdff3b505ce
Travis Walter <travis@automattic.com>
2022-06-17 18:02:38 -0700

    trivial change

--- client/src/Token.tsx
+++ client/src/Token.tsx
@@ -4,7 +4,6 @@ export default function Token() {
 	const { tokenid } = useParams();
 	console.log(tokenid);
 
-	// let tokenObject = {};
 	switch(tokenid) {
 		case '1':
 			window.location.href='/token-' + tokenid + '.json';
